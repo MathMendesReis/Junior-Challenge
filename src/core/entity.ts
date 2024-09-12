70264748288adb7bea809cbebcc1154772e61df8
Matheus Mendes <matheusmendesreis@outlook.com>
2024-09-11 22:20:25 -0300

    feat: create class base

---
+++ src/core/entity.ts
@@ -0,0 +1,19 @@
+import { UniqueEntityId } from "./unique-entity"
+
+export abstract class Entity<Props> {
+  private _id: UniqueEntityId
+  protected props: Props
+
+  get id() {
+    return this._id
+  }
+
+  protected constructor(props: Props, id?: UniqueEntityId) {
+    this._id = id ?? new UniqueEntityId()
+    this.props = props
+  }
+
+  public equals(entity: Entity<any>) {
+    return entity === this || entity._id === this.id
+  }
+}
