d4be6ddb27d1cdf6bc46070caa75612e333b9df5
Matheus Mendes <matheusmendesreis@outlook.com>
2024-09-12 00:37:03 -0300

    feat:create ring repositorie

---
+++ src/domain/ring/application/repostitories/ring-repository.ts
@@ -0,0 +1,11 @@
+import type { Ring } from "../../enterprise/model/ring";
+import type { forgedBy } from "../../enterprise/types/forgedBy";
+
+export abstract class RingRepository{
+  abstract save(data: Ring): Promise<Ring>;
+  abstract existingRings(data: forgedBy): Promise<Ring[]|null>;
+  abstract rings(): Promise<Ring[]>;
+
+
+
+}
