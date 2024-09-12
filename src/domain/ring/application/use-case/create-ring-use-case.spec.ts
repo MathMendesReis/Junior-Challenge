1268a6b6e5d2de6ac12e8f3fd963d47ba08882eb
Matheus Mendes <matheusmendesreis@outlook.com>
2024-09-12 11:22:35 -0300

    feat: create e2e test to create ring

--- src/domain/ring/application/use-case/create-ring-use-case.spec.ts
+++ src/domain/ring/application/use-case/create-ring-use-case.spec.ts
@@ -1,37 +1,42 @@
-import { expect, test,describe, beforeEach } from 'vitest'
-import { CreateRingUseCase, type CreateRingRequestDTO } from './create-ring-use-case'
-import { it } from 'node:test'
-import { InMemoryRingDatabase } from '../../../../infra/database/in-memory/in-memory-ring-database'
+import { expect, test, describe, beforeEach } from 'vitest';
+import { CreateRingUseCase, type CreateRingRequestDTO } from './create-ring-use-case';
+import { InMemoryRingDatabase } from '../../../../infra/database/in-memory/in-memory-ring-database';
 
+describe('Create Ring use case (unit)', async () => {
+  let inMemoryRepository: InMemoryRingDatabase;
+  let sut: CreateRingUseCase;
 
-describe('Create Ring use case (unit)',async ()=>{
-  const inMemoryRepository = new InMemoryRingDatabase()
-  const sut = new CreateRingUseCase(inMemoryRepository)
+  beforeEach(() => {
+    inMemoryRepository = new InMemoryRingDatabase();
+    sut = new CreateRingUseCase(inMemoryRepository);
+  });
 
   test('should create a new Ring when provided with valid input data', async () => {
-    const requestData:CreateRingRequestDTO = {
+    const requestData: CreateRingRequestDTO = {
       name: 'One Ring',
       power: 'Invisibility',
       forgedBy: 'Elfos',
-      imageURL: 'http://example.com/one-ring.jpg'
+      imageURL: 'http://example.com/one-ring.jpg',
     };
-    const result = await  sut.execute(requestData);
-    expect(result).toHaveProperty('id')
-    expect(result.getProps.name).toEqual(requestData.name)
+    const result = await sut.execute(requestData);
+    expect(result).toHaveProperty('id');
+    expect(result.getProps.name).toEqual(requestData.name);
   });
-    
-  it('should create a new ring when no existing rings are found for the given forgedBy', async () => {
 
-    const requestDTO1:CreateRingRequestDTO = {
+  test('should throw an error when the limit of rings for a race is reached (Elfos)', async () => {
+    const requestDTO: CreateRingRequestDTO = {
       name: 'Ring of Power',
       power: 'Invisibility',
       forgedBy: 'Elfos',
-      imageURL: 'http://image.url'
+      imageURL: 'http://image.url',
     };
-    // for (let index = 0; index < 2; index++) {
-    //   await  sut.execute(requestDTO1);
-    // }
-    // await expect(() => sut.execute(requestDTO1)).rejects.toThrowError(`O limite de anéis para ${requestDTO1.forgedBy} já foi atingido.`)
-  });
 
-})
+    for (let index = 0; index < 3; index++) {
+      await sut.execute(requestDTO);
+    }
+
+    await expect(() => sut.execute(requestDTO)).rejects.toThrowError(
+      'O limite de anéis para Elfos já foi atingido.'
+    );
+  });
+});
