83c2b9cc7e212d443c38d42a87581cbb043cb983
Matheus Mendes <matheusmendesreis@outlook.com>
2024-09-12 09:45:08 -0300

    feat: create method static create

--- src/domain/ring/enterprise/model/ring.ts
+++ src/domain/ring/enterprise/model/ring.ts
@@ -1,12 +1,27 @@
 import { Entity } from "../../../../core/entity";
+import type { UniqueEntityId } from "../../../../core/unique-entity";
+import type { forgedBy } from "../types/forgedBy";
 
 export interface RingProps{
-  id: string;
+  id?: string;
   name: string;
   power: string;
-  forgedBy: string;
+  forgedBy: forgedBy;
   imageURL: string;
 }
 export class Ring extends Entity<RingProps> {
 
+  static create(
+    props: RingProps,
+    id?: UniqueEntityId,
+  ) {
+    const ring = new Ring(
+      {
+        ...props,
+      },
+      id,
+    )
+
+    return ring
+  }
 }
