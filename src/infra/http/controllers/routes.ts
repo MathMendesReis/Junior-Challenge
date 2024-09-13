8cf7b03d11174e0ddbb14b6d8ca98b9ab7eec54d
Matheus Mendes <matheusmendesreis@outlook.com>
2024-09-13 06:57:18 -0300

    feat: create file with routers

--- src/infra/http/controllers/routes.ts
+++ src/infra/http/controllers/routes.ts
@@ -1,8 +1,15 @@
 import { Router } from 'express';
 import createRingController from './ring/create-ring-controller'
+import listAllRingController from './ring/list-rings-controller'
+import deleteRingByIdController from './ring/delete-ring-by-id-controller'
+
+
+
 
 
 const api = Router()
-  .use(createRingController);
+  .use(createRingController)
+  .use(deleteRingByIdController)
+  .use(listAllRingController);
 
 export default Router().use('/api', api);
