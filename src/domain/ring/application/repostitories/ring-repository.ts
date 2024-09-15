1830dfcbb9d9d399419f75584f621671b97462ad
Matheus Mendes <matheusmendesreis@outlook.com>
2024-09-15 14:56:47 -0300

    feat: update ring repository

--- src/domain/ring/application/repostitories/ring-repository.ts
+++ src/domain/ring/application/repostitories/ring-repository.ts
@@ -1,9 +1,15 @@
 import type { RingDB } from "../../../../infra/database/orm/entitie/ring-entitie";
-import type { Ring } from "../../enterprise/model/ring";
+import type { Ring, RingProps } from "../../enterprise/model/ring";
 import type { forgedBy } from "../../enterprise/types/forgedBy";
+import type { updateRingRequestDTO } from "../use-case/update-rings-use-case";
 
-export abstract class RingRepository{
-  abstract findBookByForgedBy: (forgedBy: forgedBy) => Promise<RingDB|undefined>;
-  abstract saveRing: (book: Ring) => Promise<RingDB>;
-  abstract findAllRings: () => Promise<RingDB[]>
+
+export abstract class RingRepository<T = RingDB>{
+  abstract findRingByForgedBy: (userId:string,forgedBy: forgedBy) => Promise<T[]|undefined>;
+  abstract saveRing: (ring: Ring) => Promise<T>;
+  abstract findAllRings: () => Promise<T[]>
+  abstract deleteRingById: (id:string) => Promise<void>;
+  abstract findRingById: (id:string) => Promise<T[]>;
+  abstract updateRingById: (id:string,ring: Ring) => Promise<void>;
+
 }
