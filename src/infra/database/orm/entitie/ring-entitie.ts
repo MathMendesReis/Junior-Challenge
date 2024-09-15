4a18d38f8206d11d72ed2d48b53f754f9bea3697
Matheus Mendes <matheusmendesreis@outlook.com>
2024-09-15 10:14:13 -0300

    feat: add new table

---
+++ src/infra/database/orm/entitie/ring-entitie.ts
@@ -0,0 +1,15 @@
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import type { forgedBy } from "../../../../domain/ring/enterprise/types/forgedBy";
+@Entity()
+export class RingDB {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+    @Column({ nullable: false, type:'text' })
+    name: string;
+    @Column({ nullable: false, type:'text' })
+    power: string;
+    @Column({ nullable: false, type:'text' })
+    forgedBy: forgedBy;
+    @Column({ nullable: false, type:'text' })
+    imageURL: string;
+  }
