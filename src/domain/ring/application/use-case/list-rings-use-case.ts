d8eda2ff9eddcee503506c8c9c5352f5c172c7ed
Matheus Mendes <matheusmendesreis@outlook.com>
2024-09-15 14:58:10 -0300

    feat: update use case

--- src/domain/ring/application/use-case/list-rings-use-case.ts
+++ src/domain/ring/application/use-case/list-rings-use-case.ts
@@ -1,9 +1,10 @@
+import type { RingDB } from "../../../../infra/database/orm/entitie/ring-entitie";
 import type { RingRepository } from "../repostitories/ring-repository";
 
 export class ListRingsUseCase{
-  constructor(private readonly ringRepository:RingRepository){}
+  constructor(private readonly ringRepository:RingRepository<RingDB>){}
 
   async execute() {
-    return await this.ringRepository.rings()
+    return await this.ringRepository.findAllRings()
   }
 }
