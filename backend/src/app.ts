46f546b260b91faafd41f085669934e586077182
Matheus Mendes <matheusmendesreis@outlook.com>
2024-09-16 09:06:15 -0300

    feat: add swagger

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,16 @@
 import express from 'express'
 import routes from './infra/http/controllers/routes'
-
+import swaggerUi from 'swagger-ui-express'
+const swaggerDocument = require('./swagger.json');
 
 export const app = express()
 app.get('/', (req: express.Request, res: express.Response) => {
   res.json({ status: 'API is running on /api' });
 });
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json())
 app.use(routes);
+
 
 
 
